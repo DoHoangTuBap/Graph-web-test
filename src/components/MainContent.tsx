3a6a3baecaee8634eb1c9541082a76bd4516103a
Hoang Tu <Hoangtu.programmer@gmail.com>
2024-03-19 01:00:54 +0700

    first time

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,12 @@
+// components/MainContent.js
+import React from 'react';
+
+const MainContent = () => {
+  return (
+    <main>
+      {/* Main content here */}
+    </main>
+  );
+}
+
+export default MainContent;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+// components/Navbar.js
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      {/* Navbar content here */}
+    </nav>
+  );
+}
+
+export default Navbar;
